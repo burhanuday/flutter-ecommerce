5842d649560208cc40e0e18d1396f1a5abe55001
burhanuday <udaipurwalaburhanuddin@gmail.com>
2020-05-31 18:46:37 +0530

    navigation and central state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/products_overview_screen.dart';
+import 'screens/product_detail_screen.dart';
+import 'providers/products.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,15 +11,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'My Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
+
+class ProductDetailsScreen extends StatelessWidget {
+  static const routeName = '/product-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context).findById(productId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
+  }
+}
